8d800906e862ea7ecdeff3e735fdb5634152d702
Javclamar <javclamar@alum.us.es>
2025-11-10 18:19:44 +0100

    chore: fixes isort linting warnings on the auth module

--- app/modules/auth/tests/test_selenium.py
+++ app/modules/auth/tests/test_selenium.py
@@ -1,7 +1,9 @@
 import time
+
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+
 from core.environment.host import get_host_for_selenium_testing
 from core.selenium.common import close_driver, initialize_driver
 
